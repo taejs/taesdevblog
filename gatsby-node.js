96a601ecae5c3d7098a90bf002d8a76128aff6bd
신태림 <str0308@midasit.com>
2019-05-02 20:19:52 +0900

    add markdown plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+
+exports.createPages = ({actions, graphql}) => {
+    const {createPage} = actions
+
+    const blogPostTemplate = path.resolve(`템플릿 파일`)
+
+    return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+      if(result.errors) {
+          return Promise.reject(result.errors)
+      }
+      result.data.allMarkdownRemark.edges.forEach(({node}) => {
+          cratePage({
+              path : node.frontmatter.path,
+              component : blogPostTemplate,
+              context : {}
+          })
+      })
+  })
+}
