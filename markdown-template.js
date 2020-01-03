52693c1495708c9b0dcc8600f7097228e3217c98
tae <taeshindev@gmail.com>
2020-01-03 17:34:09 +0900

    keywords추가

--- markdown-template.js
+++ markdown-template.js
@@ -4,10 +4,13 @@ import Layout from './src/components/layout';
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react"
 import { graphql } from "gatsby"
+import { Styled } from 'theme-ui'
+import SEO from './src/components/seo';
 
 export default function Template({ data: { mdx } }) {
   return (
     <Layout>
+        <SEO title={mdx.frontmatter.title} keywords={mdx.frontmatter.keywords} description={mdx.excerpt}/>
         <div class="post">
             <h1>{mdx.frontmatter.title}</h1>
             <time>{mdx.frontmatter.date}</time>
@@ -24,8 +27,10 @@ export const pageQuery = graphql`
     mdx(id: { eq: $id }) {
       id
       body
+      excerpt(pruneLength: 150)
       frontmatter {
         title
+        keywords
       }
     }
   }
